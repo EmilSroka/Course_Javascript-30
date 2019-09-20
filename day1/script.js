b14e18d10cf4714d353368cd731630d0a1c11985
Emil Sroka <srokaemil@gmail.com>
2019-09-20 12:06:58 +0200

    Add keyboard handling

--- day1/script.js
+++ day1/script.js
@@ -1,26 +1,32 @@
-const launchpad = (function Launchpad(){
-    const container = document.querySelector(".launchpad");
-    container.addEventListener("click", function TODO(event){
+(function Launchpad(){
+    const container = document.querySelector('.launchpad');
+
+    container.addEventListener('click', function playSoundWhenButton(event){
         var button = getButtonFromTarget(event.target);
         if(button == null) return;
 
         { let dataCode;
-            dataCode = button.getAttribute("data-code");
+            dataCode = button.getAttribute('data-code');
             playSound(dataCode);
         }
-
+    });
+    window.addEventListener('keydown', function  playSoundWhenCorrectKey(event){
+        var button = container.querySelector(`.launchpad__btn[data-code="${event.keyCode}"]`);
+        if(button){
+            button.classList.contains('active') ? button.classList.remove('active') : button.classList.add('active');
+            playSound(event.keyCode);
+        }
     })
-    var publicAPI = {
-
-    };
-    return publicAPI;
-
-    /* private functions */
+    container.addEventListener('transitionend', function removeTransitionEffectFromButton(event){
+        if(event.target.tagName == 'BUTTON' && event.propertyName == 'box-shadow'){
+            event.target.classList.remove('active');
+        }
+    });
 
     function getButtonFromTarget(target){
-        if(target.tagName == "ARTICLE") {
+        if(target.tagName == 'ARTICLE') {
             return null;
-        }else if(target.tagName == "BUTTON"){
+        }else if(target.tagName == 'BUTTON'){
             return target;
         } else{
             return target.parentNode;
